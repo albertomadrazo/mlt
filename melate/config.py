e0b1abb65ab2e64382111566bdaef6532bcd4213
Desarrollo Web <Desarrollo Web>
2017-09-18 15:49:25 -0500

    Ignora el archivo secrets.

--- melate/config.py
+++
@@ -1,2 +0,0 @@
-import os
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
--- melate/secrets.py
+++
@@ -1,4 +0,0 @@
-my_db = 'melate'
-my_secret_key = 'TIOJGGTJEWRGUJIAGAFD95459084tjgreoiji//(Ijhflf'
-my_user = 'root'
-my_password = 'pendulo'
