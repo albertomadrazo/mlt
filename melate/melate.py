53cdf30c158148e6ee972d94ef8f38181bc3bba8
Alberto Madrazo <betomadrazo@gmail.com>
2017-09-22 12:11:32 -0500

    Formatea la fecha a algo bonito.

--- melate/melate.py
+++ melate/melate.py
@@ -1,4 +1,7 @@
 import os
+import locale
+from time import strftime
+import datetime
 
 from flask_mysqldb import MySQL
 
@@ -6,6 +9,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 from .secrets import my_db, my_secret_key, my_user, my_password
 
+locale.setlocale(locale.LC_ALL, "es_MX.utf8")
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -26,17 +30,39 @@ mysql = MySQL(app)
 @app.route('/')
 def sorteos():
 	cur = mysql.connection.cursor()
-	cur.execute("SELECT * FROM melate LIMIT 20")
-	# rv = cur.fetchall()
+	cur.execute("SELECT * FROM melate LIMIT 3")
 	concursos = cur.fetchall()
 
-	# for ja in rv:
-	# 	print(ja)
-	# 	print('\n')
-
-	# print(concursos)
+	for c in concursos:
+		fecha_formateada = strftime("%A %d de %B, %Y", c['FECHA_CONCURSO'].timetuple())
+		c['FECHA_CONCURSO'] = fecha_formateada.decode('utf8')
 
 	return render_template('index.html', concursos=concursos)
+
+
+@app.route('/numero')
+def numero():
+	numero = request.args.get('n')
+
+	# obtener las repeticiones de n
+	# obtener la ultima ocasion en que salio n
+
+	ocurrencias = 0
+
+	cur = mysql.connection.cursor()
+	query = "SELECT COUNT(*) FROM melate WHERE R1='"+numero+"' OR R2='"+numero+"' OR R3='"+numero+"' OR R4='"+numero+"' OR R5='"+numero+"' OR R6='"+numero+"' OR R7='"+numero+"'"
+	print(query)
+	cur.execute(query)
+	historia_numero = cur.fetchall()[0]['COUNT(*)']
+
+	# for num in historia_numero:
+	# 	print(num['R1'])
+		# if num['R1'] == string(numero)
+
+	# print(historia_numero[0]['COUNT(*)'])
+	# print type(historia_numero)
+	return render_template('numero.html', historia_numero=historia_numero)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
