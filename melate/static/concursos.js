fdc591a9ad3ec641ae098fb96625ee77a647c149
Alberto Madrazo <betomadrazo@gmail.com>
2017-09-24 22:17:56 -0500

    Limpia el archivo.

--- melate/static/concursos.js
+++ melate/static/concursos.js
@@ -2,9 +2,6 @@ $(function(){
     var moveLeft = 20; 
     var moveDown = 10;
 
-    // $('.pelota').on('mouseover', function(){
-    // });
-
     $('.pelota').hover(function(event){
         var numero = $(this).text();
         var numeroOver = $(this).find('span').html();
@@ -12,14 +9,8 @@ $(function(){
             $('.num-stats-cont').show();
             $('.num-stats-cont div:first-of-type').html(numero);
             $('.ocurrencias').html(data);
-
-            console.log('---> ' + data);
         });
-        
-
-    }, function(){
-        $('.num-stats-cont').hide();
-    });
+    }, function(){ $('.num-stats-cont').hide(); });
 
     $('.pelota').mousemove(function(event){
         $('.num-stats-cont').css({'top':event.pageY + moveDown, 'left':event.pageX + moveLeft});
