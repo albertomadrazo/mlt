f2975ea5c1e18d8a97350896ade912a72a02b7df
Alberto Madrazo <otreblatercero@gmail.com>
2017-09-27 16:42:54 -0500

    Mas refactorizacion y actualiza la tabla con los sorteos que le falten.

--- get_historico_melate.py
+++ get_historico_melate.py
@@ -25,17 +25,24 @@ def convert_csv_to_list(archivo_destino):
     lista_concursos = []    
 
     for r in reversed(list(registros.readlines())):
-        r = r.replace('\n', '').split(',')
-        try:
-            r[-1] = datetime.datetime.strptime(r[-1], '%d/%m/%Y').date()
-            lista_concursos.append(r)
-        except ValueError:
-            pass
+        lista_concursos.append(format_line_to_list(r))
 
     return lista_concursos
 
 
+def format_line_to_list(line):
+    line = line.replace('\n', '').split(',')
+    try:
+        line[-1] = datetime.datetime.strptime(line[-1], '%d/%m/%Y').date()
+        return line
+    except ValueError:
+        return []    
+
+
 def get_csv_from_url(url, archivo_destino):
+    """
+    Descarga el archivo CSV del sitio de pronósticos
+    """
     if not os.path.exists(archivo_destino):
         with urllib.request.urlopen(url) as response, open(archivo_destino, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
@@ -45,19 +52,22 @@ def get_csv_from_url(url, archivo_destino):
     return True
 
 
-def llenar_tabla_vacia(lista_concursos):
+def agregar_records_a_tabla(lista_concursos):
     clave_melate = 40
     for lista in lista_concursos:
-        concurso = int(lista[1])
-        num1 = int(lista[2])
-        num2 = int(lista[3])
-        num3 = int(lista[4])
-        num4 = int(lista[5])
-        num5 = int(lista[6])
-        num6 = int(lista[7])
-        num7 = int(lista[8])
-        bolsa = int(lista[9])
-        fecha = lista[10]
+        try:
+            concurso = int(lista[1])
+            num1 = int(lista[2])
+            num2 = int(lista[3])
+            num3 = int(lista[4])
+            num4 = int(lista[5])
+            num5 = int(lista[6])
+            num6 = int(lista[7])
+            num7 = int(lista[8])
+            bolsa = int(lista[9])
+            fecha = lista[10]
+        except IndexError:
+            pass
 
         try:
             with connection.cursor() as cursor:
@@ -65,10 +75,8 @@ def llenar_tabla_vacia(lista_concursos):
                 query += "producto, concurso, num1, num2, num3, num4, num5, num6, num7, bolsa, fecha)"
                 query += " VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                 cursor.execute(query, (clave_melate, concurso, num1, num2, num3, num4, num5, num6, num7, bolsa, fecha))
-                # Funcionará si le doy commit en el finally ?
-                connection.commit()
         finally:
-            pass
+            connection.commit()
 
 
 def get_concurso():
@@ -80,15 +88,55 @@ def get_concurso():
             if not result:
                 return False
             else:
-                return result
+                return result[0]['concurso']
     finally:
         pass
 
 
-def actualizar_tabla():
-    concurso = get_concurso()
-    if concurso == True:
-        print(concurso)
+def actualizar_tabla(url, archivo_csv):
+    """
+    Actualiza la tabla melate2 si el último concurso en el CSV es mayor al
+    que está en la tabla.
+    """
+    concurso_csv = checa_ultimo_concurso_de_csv(url, archivo_csv)
+    concurso_reciente = get_concurso()
+
+    if concurso_csv == concurso_reciente:
+        print("El concurso {} es el más reciente en la base de datos de melate.".format(concurso_csv))
+    elif int(concurso_csv) > concurso_reciente:
+        lista_concursos = []
+        # Iterar el archivo CSV desde el concurso que tenemos en nuestra db
+        archivo = open(archivo_csv)
+        for i, linea in enumerate(list(archivo.readlines())):
+            if i == 0:
+                continue
+
+            # Formatear cada linea y meter el contenido en una lista
+            tmp = format_line_to_list(linea)
+            if int(tmp[1]) > concurso_reciente:
+                lista_concursos.insert(0, tmp)
+            else:
+                break
+
+        agregar_records_a_tabla(lista_concursos)
+
+
+def checa_ultimo_concurso_de_csv(url, archivo_csv):
+    existe = False
+    if not os.path.exists(archivo_csv):
+        if get_csv_from_url(url, archivo) == True:
+            existe = True
+        else:
+            print('No se puede acceder al archivo CSV.')
+    else:
+        existe = True
+
+    if existe == True:
+        archivo = open(archivo_csv)
+        ultimo_concurso = archivo.readlines()[1].split(',')[1]
+        return ultimo_concurso
+
+    return existe
 
 
 def main():
@@ -99,12 +147,12 @@ def main():
     if get_concurso() == False:
         print('tabla vacía, se llenará con el último reporte.')
         if get_csv_from_url(direccion_csv, archivo_destino) == True:
-            llenar_tabla_vacia(convert_csv_to_list(archivo_destino))
+            agregar_records_a_tabla(convert_csv_to_list(archivo_destino))
+            print('listo!')
         else:
             print('No se ha podido encontrar un archivo CSV en la locación definida.')
     else:
-        actualizar_tabla()
-        print(get_concurso())
+        actualizar_tabla(direccion_csv, archivo_destino)
 
 
 
